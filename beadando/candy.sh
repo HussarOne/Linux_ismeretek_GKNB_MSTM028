0e9076fd2cfe67c741ff0156836ba2cc8d23de85
HussarOne <101748708+HussarOne@users.noreply.github.com>
2023-06-12 20:02:17 +0200

    szöveg kiírás centerelve megoldva

--- beadando/candy.sh
+++ beadando/candy.sh
@@ -11,79 +11,46 @@ minSzel=64  #pálya max szélessége 15 x 15-nél
 minMag=34   #lehet hogy csak 32 dunno, majd alaposabban átszámolom
 
 if [[ $width -lt $minSzel ]] || [[ $height -lt $minMag ]]; then
-    midWidth=$((width/2 + 1))
+    midWidth=$((width/2))
     midHeight=$((height/2))
+    printf "%s %d \n" "$midWidth" "$midHeight"
 
     if [[ $width -gt 20 ]] || [[ $height -gt 3 ]]; then #az az ág ahol van hely kiírni a tájékoztatást
-        msg_egy="A terminálnak minimum\n"   
-        msg_ketto=" 34  magasság\n"
-        msg_harom=" 64 szélesség\n"
-        msg_negy="kell!"          
+        msg="A terminálnak minimum 34 magasságra 64 szélességre van szüksége"
+        
+        msg_egy="A terminálnak minimum"   
+        msg_ketto=" 34  magasság"
+        msg_harom=" 64 szélesség"
+        msg_negy=" kell!"          
 
         startPoz_egy=$((midWidth-(${#msg_egy}/2)))
         startPoz_ketto=$((midWidth-(${#msg_ketto}/2)))
         startPoz_harom=$((midWidth-(${#msg_harom}/2)))
         startPoz_negy=$((midWidth-(${#msg_negy}/2)))
 
-        #\033[L;CH 
+        printf "%s %s %s %s \n" "$startPoz_egy" "$startPoz_ketto" "$startPoz_harom" "$startPoz_negy"
+        #echo -en "\033[$((midHeight-3));$((startPoz_egy))H"
+        #echo "test behúzás"
+
+        #\033[L;CH  $startPoz_egy
         #ezen a ponton még mindig tiszta a terminál a kezdeti törlés után!
         #nem kell 0,0-ra állítani a kurzort, hisz ott van!
-        #echo -en "\033[0;0H"  
+        #echo -en "\033[4;$((szam-3))H"
         #\033[1A down
         #\033[1C forward
 
-        for((j = 0; j < midHeight - 2; j++))  #bemozgatás a magasság közepére
-        do
-            echo -en "\033[1A"
-        done
-
-        for((i = 0; i < startPoz_egy; i++))
-        do
-            echo -en "\033[1C"       #sorban való betekerés helyére
-        done
-        printf "%s" "$msg_egy"
-        echo -en "\033[0;0H"         #visszamozgatom 0,0-ba a kurzort
-
+        #bemozgatás a magasság közepe előtti 2. sorra, kezdő pozira a sorban
+        echo -en "\033[$((midHeight-1));$((startPoz_egy))H"  
+        printf "%s\n" "$msg_egy"
+    
+        echo -en "\033[$((midHeight));$((startPoz_ketto))H" 
+        printf "%s\n" "$msg_ketto"
         
-
-        for((j = 0; j < midHeight - 1; j++))  #bemozgatás a magasság közepére
-        do
-            echo -en "\033[1A"
-        done
-
-        for((i = 0; i < startPoz_ketto; i++))
-        do
-            echo -en "\033[1C"      #sorban való betekerés helyére
-        done
-        printf "%s" "$msg_ketto"
-        echo -en "\033[0;0H"         #visszamozgatom 0,0-ba a kurzort
-
-
-
-        for((j = 0; j < midHeight; j++))  #bemozgatás a magasság közepére
-        do
-            echo -en "\033[1A"
-        done
-
-        for((i = 0; i < startPoz_harom; i++))
-        do
-            echo -en "\033[1C"      #sorban való betekerés helyére
-        done
-        printf "%s" "$msg_harom"
-        echo -en "\033[0;0H"         #visszamozgatom 0,0-ba a kurzort
-
-
-
-        for((j = 0; j < midHeight + 1; j++))  #bemozgatás a magasság közepére
-        do
-            echo -en "\033[1A"
-        done
-
-        for((i = 0; i < startPoz_negy; i++))
-        do
-            echo -en "\033[1C"      #sorban való betekerés helyére
-        done
-        printf "%s" "$msg_negy"
+        echo -en "\033[$((midHeight+1));$((startPoz_harom))H" 
+        printf "%s\n" "$msg_harom"
+        
+        echo -en "\033[$((midHeight+2));$((startPoz_negy))H" 
+        printf "%s\n" "$msg_negy"
 
     else                                                #az az ág ahol nincs hely kiírni a tájékoztatást
         echo "A játéknak több helyre van szüksége a terminálon!"
@@ -156,59 +123,32 @@ do
 done
 printf "\n"
 
-fixPos_X=12              #ideálisan a 2. karakterre szeretnénk helyezni mögé a kocsit 
+fix_X=12              #ideálisan a 2. karakterre szeretnénk helyezni mögé a kocsit 
 startPos=8               #8 az első opció!
 maxPos=12                #ennél tovább nem mehet
 minPos=$startPos         #elején ez a minimum is
 aktualPos=$startPos      #kezetben 8-ra rakjuk, így a startposnál van!
 
-#ezt ez alatt kipróbálni printf-el és beszúrással
-echo -en "\033[$startPos;12H"            #Kocsi hátramozgatása, így nem írok bele a szövegbe
-read -r -n 1 char                        #ciklust indító kezdő beolvasás
-#echo $char                              #csupán teszteléshez ellenőrzés volt
-while [[ $char != "" ]]; do 
-    #printf "%s /t" "${char[1]}"         #13 sor van 
-    if [[ $char = "w" ]]; then
-        if [[ $((aktualPos-1)) -ge minPos ]]; then
-            echo -en "\033[$aktualPos;11H"      #1 karakterrel előrébb lépés, hogy a törlés elkapja a most beírt karaktert
-            echo -en "\033[K"                   #sorvég törlése ami kocsi után van
+echo -en "\033[$startPos;$((fix_X))H"            #Kocsi hátramozgatása, így nem írok bele a szövegbe
+read -rsn 1 char                         #ciklust indító kezdő beolvasás
 
+while [[ $char != "" ]]; do 
+   if [[ $char = "w" ]]; then
+        if [[ $((aktualPos-1)) -ge minPos ]]; then
             aktualPos=$((aktualPos-1))          #pozíció értékének csökkentése, ha még tudunk felfele lépni!           
-            echo -en "\033[1A"                  #1 sorral feljebb ugrás
-            echo -en "\033[$aktualPos;12H"      #visszalépés a 12-es X koordinátára ugyan abban a sorban
-        
-        else                    #az az ág, ha nincs hely felfele!
-            echo -en "\033[$aktualPos;11H"      #1 karakterrel előrébb lépés, hogy a törlés elkapja a most beírt karaktert
-            echo -en "\033[K"                   #sorvég törlése ami kocsi után van
-            echo -en "\033[$aktualPos;12H"      #visszalépés a 12-es X koordinátára ugyan abban a sorban
+            echo -en "\033[$aktualPos;$((fix_X))H"      #visszalépés a 12-es X koordinátára ugyan abban a sorban
         fi
     fi
 
     if [[ $char = "s" ]]; then
         if [[ $((aktualPos+1)) -le maxPos ]]; then
-            echo -en "\033[$aktualPos;11H"      #1 karakterrel előrébb lépés, hogy a törlés elkapja a most beírt karaktert
-            echo -en "\033[K"                   #sorvég törlése ami kocsi után van
-
             aktualPos=$((aktualPos+1))          #pozíció értékének csökkentése, ha még tudunk felfele lépni!           
-            echo -en "\033[1B"                  #1 sorral lejebb ugrás
-            echo -en "\033[$aktualPos;12H"      #visszalépés a 12-es X koordinátára ugyan abban a sorban
-        
-        else                    #az az ág, ha nincs hely lefele!
-            echo -en "\033[$aktualPos;11H"      #1 karakterrel előrébb lépés, hogy a törlés elkapja a most beírt karaktert
-            echo -en "\033[K"                   #sorvég törlése ami kocsi után van
-            echo -en "\033[$aktualPos;12H"      #visszalépés a 12-es X koordinátára ugyan abban a sorban
+            echo -en "\033[$aktualPos;$((fix_X))H"      #visszalépés a 12-es X koordinátára ugyan abban a sorban
         fi
     fi
 
-    if [[ $char != "w" ]] && [[ $char != "s" ]]; then
-        echo -en "\033[$aktualPos;11H"      #1 karakterrel előrébb lépés, hogy a törlés elkapja a most beírt karaktert
-        echo -en "\033[K"                   #sorvég törlése ami kocsi után van
-        echo -en "\033[$aktualPos;12H"      #visszalépés a 12-es X koordinátára ugyan abban a sorban
-    fi
-
-    read -r -n 1 char
+    read -rsn 1 char
 done
 
 echo -e  "clear; \033c\e[3J"         #képernyő letisztítása
 echo -en "\033[1A"                   #kocsi feljebb ugratása 1-el 
-printf "kijutottam enterrel! \n"
