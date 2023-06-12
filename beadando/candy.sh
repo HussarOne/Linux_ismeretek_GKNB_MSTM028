e404f094422371e2b0012aad56d05e01e2365257
HussarOne <101748708+HussarOne@users.noreply.github.com>
2023-06-12 02:19:00 +0200

    Sikeres kurzor mozgatás
    -sikeresen mozgatja min és max pozik között a kurzort
    -sikeres törlés, ugratás, és visszahelyezés
    -beírt karakterek alig láthatóak
    -érzékeli az entert

--- beadando/candy.sh
+++ beadando/candy.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-echo -e 'clear; \033c\e[3J'            #képernyő letisztítása
-echo -e " "                             #képernyő szándékos teleszemetelése
-echo -e 'clear; \033c\e[3J'            #ismételt tisztítás
+echo -e  "clear; \033c\e[3J"         #képernyő letisztítása
+echo -en "\033[1A"                   #kocsi feljebb ugratása 1-el 
 
 #név beolvasása
 printf "Add meg a neved! \nneved: " 
 read -r username
-#printf "%s \n" "$username"         #tesztelésre kell csak
 
 #pálya elkészítéshez információk
 printf "\nNavigálni a W: felfele és S:lefele gombokkal lehet!"
 printf "\nPályaméret választáshoz navigálj a kívánt számra és ENTER!"
-printf "\nPályaméret lehetőségek:"
+
+printf "\n\nPályaméret lehetőségek:"
 minMeret=11                         #pálya minimum mérete, szimmetrikus
 enlarge=2                           #opcióknál ennyivel növeljük a méretet szimmetrikusan
 aktualMeret=$minMeret               #ez az aktuális méret amit kiírunk választhatónak
@@ -32,27 +31,62 @@ printf "\n"
 #    
 #done
 
-echo -en "\033[1;15H"                    #Kocsi hátramozgatása, így nem írok bele a szövegbe
+fixPos_X=12              #ideálisan a 2. karakterre szeretnénk helyezni mögé a kocsit 
+startPos=8               #8 az első opció!
+maxPos=12                #ennél tovább nem mehet
+minPos=$startPos         #elején ez a minimum is
+aktualPos=$startPos      #kezetben 8-ra rakjuk, így a startposnál van!
+
+#ezt ez alatt kipróbálni printf-el és beszúrással
+echo -en "\033[$startPos;12H"            #Kocsi hátramozgatása, így nem írok bele a szövegbe
 read -r -n 1 char                        #ciklust indító kezdő beolvasás
 #echo $char                              #csupán teszteléshez ellenőrzés volt
 while [[ $char != "" ]]; do 
-    #printf "%s /t" "${char[1]}"
+    #printf "%s /t" "${char[1]}"         #13 sor van 
     if [[ $char = "w" ]]; then
-        echo -en "\033[1A"
-        echo -en "\033[1;15H"
+        if [[ $((aktualPos-1)) -ge minPos ]]; then
+            echo -en "\033[$aktualPos;11H"      #1 karakterrel előrébb lépés, hogy a törlés elkapja a most beírt karaktert
+            echo -en "\033[K"                   #sorvég törlése ami kocsi után van
+
+            aktualPos=$((aktualPos-1))          #pozíció értékének csökkentése, ha még tudunk felfele lépni!           
+            echo -en "\033[1A"                  #1 sorral feljebb ugrás
+            echo -en "\033[$aktualPos;12H"      #visszalépés a 12-es X koordinátára ugyan abban a sorban
+        
+        else                    #az az ág, ha nincs hely felfele!
+            echo -en "\033[$aktualPos;11H"      #1 karakterrel előrébb lépés, hogy a törlés elkapja a most beírt karaktert
+            echo -en "\033[K"                   #sorvég törlése ami kocsi után van
+            echo -en "\033[$aktualPos;12H"      #visszalépés a 12-es X koordinátára ugyan abban a sorban
+        fi
     fi
 
     if [[ $char = "s" ]]; then
-        echo -en "\033[1B"
-        echo -en "\033[1;15f"
+        if [[ $((aktualPos+1)) -le maxPos ]]; then
+            echo -en "\033[$aktualPos;11H"      #1 karakterrel előrébb lépés, hogy a törlés elkapja a most beírt karaktert
+            echo -en "\033[K"                   #sorvég törlése ami kocsi után van
+
+            aktualPos=$((aktualPos+1))          #pozíció értékének csökkentése, ha még tudunk felfele lépni!           
+            echo -en "\033[1B"                  #1 sorral lejebb ugrás
+            echo -en "\033[$aktualPos;12H"      #visszalépés a 12-es X koordinátára ugyan abban a sorban
+        
+        else                    #az az ág, ha nincs hely lefele!
+            echo -en "\033[$aktualPos;11H"      #1 karakterrel előrébb lépés, hogy a törlés elkapja a most beírt karaktert
+            echo -en "\033[K"                   #sorvég törlése ami kocsi után van
+            echo -en "\033[$aktualPos;12H"      #visszalépés a 12-es X koordinátára ugyan abban a sorban
+        fi
     fi
 
     if [[ $char != "w" ]] && [[ $char != "s" ]]; then
-        echo -en "\033[K"
+        echo -en "\033[$aktualPos;11H"      #1 karakterrel előrébb lépés, hogy a törlés elkapja a most beírt karaktert
+        echo -en "\033[K"                   #sorvég törlése ami kocsi után van
+        echo -en "\033[$aktualPos;12H"      #visszalépés a 12-es X koordinátára ugyan abban a sorban
     fi
 
     read -r -n 1 char
 done
+
+echo -e  "clear; \033c\e[3J"         #képernyő letisztítása
+echo -en "\033[1A"                   #kocsi feljebb ugratása 1-el 
+printf "kijutottam enterrel! \n"
 
 #\033[L;CH          kocsi pozicióba mozgatása, Sor; Oszlop koordináták
 #\033[L;Cf          ugyan az de más a vége, ezt is kipróbálom
